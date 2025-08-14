f70991b1be26196d67c8c1fd5cbf76fab1563419
Abhinit Rai <abhinit1990@gmail.com>
2025-08-14 23:14:03 +0530

    jewels and stones using Set Data Structure

--- jewelsAndStones.js
+++ jewelsAndStones.js
@@ -1,6 +1,27 @@
+//using native JS array methods
+// var numJewelsInStones = function(jewels, stones) {
+//     let jewelCount = 0;
+//     stones.split("").forEach((stone)=>{
+//         jewels.includes(stone) ? jewelCount++ : null
+//     })
+// }
+
+//using Set data structure to better the time complexity
 var numJewelsInStones = function(jewels, stones) {
     let jewelCount = 0;
-    stones.split("").forEach((stone)=>{
-        jewels.includes(stone) ? jewelCount++ : null
-    })
-}
+    let jSet = new Set();
+
+    for(let i=0;i<jewels.length;i++){
+        jSet.add(jewels[i])
+    } 
+
+    for(let j=0; j<stones.length;j++){
+        if(jSet.has(stones[j])){
+            jewelCount++
+        }
+    }
+   
+
+    return jewelCount;
+
+};
