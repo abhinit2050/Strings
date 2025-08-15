c8bbd1457fb830903e0cbae76815fa47873c9e75
Abhinit Rai <abhinit1990@gmail.com>
2025-08-15 09:00:13 +0530

    improved vowels and consonants count

--- maxVowelsConsonants.js
+++ maxVowelsConsonants.js
@@ -13,14 +13,27 @@ var maxFreqSum = function(s) {
     let maxConsonant=0;
     let vowelsArray=['a','e','i','o','u'];
 
-    for(let j=0; j<s.length;j++){
-        if(vowelsArray.includes(s[j])){
-            if(map[s[j]]>maxVowel){
-                maxVowel = map[s[j]]
+    // for(let j=0; j<s.length;j++){
+    //     if(vowelsArray.includes(s[j])){
+    //         if(map[s[j]]>maxVowel){
+    //             maxVowel = map[s[j]]
+    //         }
+    //     } else {
+    //         if(map[s[j]]>maxConsonant){
+    //             maxConsonant = map[s[j]]
+    //         }
+    //     }
+    // }
+
+    //iterating just over the map object => lesser number of iterations
+    for(let j=0; j<mapKeys.length;j++){
+        if(vowelsArray.includes(mapKeys[j])){
+            if(map[mapKeys[j]]>maxVowel){
+                maxVowel = map[mapKeys[j]]
             }
         } else {
-            if(map[s[j]]>maxConsonant){
-                maxConsonant = map[s[j]]
+            if(map[mapKeys[j]]>maxConsonant){
+                maxConsonant = map[mapKeys[j]]
             }
         }
     }
