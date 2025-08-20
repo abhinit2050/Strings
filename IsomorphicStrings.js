a4a2747242bfad1d968debe602ef2824331a9e54
Abhinit Rai <abhinit1990@gmail.com>
2025-08-20 07:03:53 +0530

    isomorphic test using two objects

--- IsomorphicStrings.js
+++ IsomorphicStrings.js
@@ -1,17 +1,37 @@
-var isIsomorphic = function(s, t) {
-    var obj = {};
+
+// var isIsomorphic = function(s, t) {
+//     var obj = {};
+
+//     for(let i=0;i<s.length;i++){
+       
+//         if(!obj.hasOwnProperty(s[i]) && !Object.values(obj).includes(t[i])){
+//             obj[s[i]] = t[i]
+//         } else if(obj[s[i]] != t[i] ){
+//             return false
+//         }
+//     }
+
+//     return true;
+// };
+
+
+//alternate approach
+var isIsomorphic = function(s,t){
+    let map1={};
+    let map2={};
 
     for(let i=0;i<s.length;i++){
-       
-        if(!obj.hasOwnProperty(s[i]) && !Object.values(obj).includes(t[i])){
-            obj[s[i]] = t[i]
-        } else if(obj[s[i]] != t[i] ){
-            return false
+        if(!map1[s[i]] && !map2[t[i]]){
+            map1[s[i]] = t[i];
+            map2[t[i]] = s[i];  
+        } else if(map2[t[i]] != s[i]){
+            return false;
+        } else if(map1[s[i]] != t[i]){
+            return false;
         }
     }
-
     return true;
-};
+}
 
 let s="paper";
 let t="title";
